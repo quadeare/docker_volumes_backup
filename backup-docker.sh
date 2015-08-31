bb5df1f4611beba8b25ae0563a2144304a49c3e7
Florian Lacrampe <quadeare@users.noreply.github.com>
2015-08-31 22:06:30 +0200

    Initial script

---
+++ backup-docker.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# This script allows you to backup all volumes from a container in a tar.gz file
+
+CONTAINER_NAME=$1
+VOLUMES=$(docker inspect --format='{{range $vol, $path := .Volumes}}{{$vol}} {{end}}' $CONTAINER_NAME)
+DATE=$(date +"%d-%b-%Y_%H-%M-%S")
+FILENAME=$CONTAINER_NAME"_"$DATE.tar.gz
+
+echo "Backup docker's volume(s) from : "$CONTAINER_NAME
+
+usage() {
+  echo "Usage: $0 [container name]"
+  exit 1
+}
+
+if [ -z $CONTAINER_NAME ]
+then
+  echo "Error: missing container name parameter."
+  usage
+fi
+
+printf "\n"
+echo "Pause docker container : "$CONTAINER_NAME
+docker pause $CONTAINER_NAME >> /dev/null
+
+echo "Backup datas from "$CONTAINER_NAME" container"
+docker run --rm --volumes-from $CONTAINER_NAME -v $(pwd):/backup ubuntu tar -P -czf /backup/$FILENAME $VOLUMES >> /dev/null
+
+echo "Unpause docker container : "$CONTAINER_NAME
+docker unpause $CONTAINER_NAME >> /dev/null
+
+echo "Output file name : "$FILENAME
+
+echo "Done."
